0dab40c68b04ce4667cad6a7b088d2221c1abe88
MyaGwen <wendy.e.ahadome@gmail.com>
2023-04-05 05:23:46 -0700

    commit

--- react-google-homepage/src/components/AppIcon.jsx
+++ react-google-homepage/src/components/AppIcon.jsx
@@ -3,8 +3,8 @@ import Icon from '../Images/googlelogo.png'
 
 export const AppIcon = (props) => {
   return (
-    <a href="#">
-    <img  src={Icon} alt="Google Logo" />
+    <a href="#" >
+    <img  className='appicon' src={Icon} alt="Google Logo" />
   </a> 
    );
 }
